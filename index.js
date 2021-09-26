6352dd8b5fb8268fffa5fe4777508cc875215538
Rabire <rabireh@outlook.fr>
2021-09-26 13:41:49 +0200

    login ok

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("ok");
+const puppeteer = require("puppeteer");
+
+const usersData = [
+  {
+    credentials: {
+      username: "rabireh@outlook.fr",
+      password: "rabire",
+    },
+    lessons: {
+      monday: [{ name: "BoxeAnglaise", startHour: "17h45" }],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: [],
+      saturaday: [],
+      sunday: [],
+    },
+  },
+];
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--window-size=1000,800"],
+    defaultViewport: null,
+  });
+  const page = await browser.newPage();
+
+  await page.goto(
+    `https://www.multiresa.net/sports/club/club-rhone/index.php?mod=membre&action=connexion`,
+    {
+      waitUntil: "networkidle0",
+    }
+  );
+
+  for (const user of usersData) {
+    await page.type("input[name=login]", user.credentials.username);
+    await page.type("input[name=password]", user.credentials.password);
+    await page.click('input[type="submit"]');
+  }
+
+  // await page.click('button[class="aOOlW  bIiDR  "]'); // accept button
+})();
