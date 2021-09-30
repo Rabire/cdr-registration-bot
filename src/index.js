adeb50f62046259aea4f09c119d5a4f985e5d109
Rabire <rabireh@outlook.fr>
2021-09-30 11:00:41 +0200

    static-data

--- src/index.js
+++ src/index.js
@@ -2,34 +2,7 @@ const puppeteer = require("puppeteer");
 const { getDay } = require("date-fns");
 
 const usersData = require("./data-config.json").usersData;
-
-const dayNames = [
-  "sunday",
-  "monday",
-  "tuesday",
-  "wednesday",
-  "thursday",
-  "friday",
-  "saturaday",
-];
-
-const lessonsId = {
-  "BOXE ANGLAISE": "MENUa-92",
-  "BOXE THAILANDAISE": "MENUa-93",
-  "JUJITSU BRESILIEN": "MENUa-106",
-  "BOXE FRANCAISE": "MENUa-94",
-  "BOXE AMERICAINE": "MENUa-95",
-  "CROSS TRAINING": "MENUa-97",
-  "KICK BOXING": "MENUa-103",
-  "KRAV MAGA": "MENUa-105",
-  "MMA FREE FIGHT": "MENUa-107",
-  GRAPPLING: "MENUa-98",
-  "JEET KUNE DO": "MENUa-99",
-  "KALI ESKRIMA": "MENUa-100",
-  CAPOEIRA: "MENUa-96",
-  JUDO: "MENUa-102",
-  "SELF DEFENSE CIVIL": "MENUa-108",
-};
+const { lessonsId, dayNames } = require("./static-data");
 
 (async () => {
   const browser = await puppeteer.launch({
---
+++ src/static-data.js
@@ -0,0 +1,27 @@
+exports.lessonsId = {
+  "BOXE ANGLAISE": "MENUa-92",
+  "BOXE THAILANDAISE": "MENUa-93",
+  "JUJITSU BRESILIEN": "MENUa-106",
+  "BOXE FRANCAISE": "MENUa-94",
+  "BOXE AMERICAINE": "MENUa-95",
+  "CROSS TRAINING": "MENUa-97",
+  "KICK BOXING": "MENUa-103",
+  "KRAV MAGA": "MENUa-105",
+  "MMA FREE FIGHT": "MENUa-107",
+  GRAPPLING: "MENUa-98",
+  "JEET KUNE DO": "MENUa-99",
+  "KALI ESKRIMA": "MENUa-100",
+  CAPOEIRA: "MENUa-96",
+  JUDO: "MENUa-102",
+  "SELF DEFENSE CIVIL": "MENUa-108",
+};
+
+exports.dayNames = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturaday",
+];
